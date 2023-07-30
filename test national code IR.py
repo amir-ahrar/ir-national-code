43cfb86a4684f17583ef65abc512ae7a68b7d3bd
amir-ahrar <140323608+amir-ahrar@users.noreply.github.com>
2023-07-30 23:56:43 +0330

    Add files via upload
    Python code to test the authenticity of the Iranian national code

---
+++ test national code IR.py
@@ -0,0 +1,14 @@
+def test_national_code(code):
+    code=str(code)
+    if not(code.isnumeric()) or len(code)!=10 or code=="0000000000":
+        return False
+    else:
+        Sum=0
+        for i in range (0,9):
+            Sum+=int(code[i])*(10-i)
+        if Sum%11<2 and int(Sum%11)==int(code[9]):
+            return True
+        elif int(11-(Sum%11))==int(code[9]):
+            return True
+        else :return False
+
